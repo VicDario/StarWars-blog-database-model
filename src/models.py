377af5c3337157af6811d4bf6f215c8950c80fd7
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-07 20:33:16 +0000

    Finished

--- src/models.py
+++ src/models.py
@@ -20,6 +20,8 @@ class Favorites(Base):
     __tablename__ = 'Favorite'
     id = Column(Integer, primary_key=True)
     id_user = Column(Integer, ForeignKey('user.id'))
+    id_favorite = Column(Integer, nullable=False)
+    type_favorite = Column(String(20), nullable=False)
     name = Column(String(100), nullable=False)
     user = relationship(User)
 
@@ -47,6 +49,38 @@ class Characters(Base):
     height = Column(String(4))
     home_world_id = Column(Integer, ForeignKey('planet.id'))
     planet = relationship(Planet)
+
+class Species(Base):
+    __tablename__ = 'specie'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    classification = Column(String(50), nullable=False)
+    designation = Column(String(50), nullable=False)
+    average_height = Column(Integer, nullable=False)
+    average_lifespan = Column(Integer, nullable=False)
+    hair_colors = Column(String(100), nullable=False)
+    skin_colors = Column(String(100), nullable=False)
+    homeworld = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+class Starships(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    model = Column(String(100), nullable=False)
+    starship_class = Column(String(100), nullable=False)
+    manufacturer = Column(String(100), nullable=False)
+    cost_in_credits = Column(Integer, nullable=False)
+    length = Column(Integer, nullable=False)
+    crew = Column(Integer, nullable=False)
+    passengers = Column(Integer, nullable=False)
+    max_atmosphering_speed = Column(String(30), nullable=False)
+    hyperdrive_rating = Column(String(10), nullable=False)
+    MGLT = Column(String(5), nullable=False)
+    cargo_capacity = Column(String(30), nullable=False)
+    consumables = Column(String(20), nullable=False)
+    pilots = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Characters, backref="starchip")
 
 
 ## Draw from SQLAlchemy base
